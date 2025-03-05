db4b1e00fe1a2e51e461a2be7af2c486da6221fa
Dream_startooo <14820205+dreamstartooo@user.noreply.gitee.com>
2025-03-05 17:44:14 +0800

    Site updated: 2025-03-05 17:44:13

--- js/weather.js
+++ js/weather.js
@@ -1,12 +1,12 @@
 (function(a,h,g,f,e,d,c,b){b=function(){d=h.createElement(g);c=h.getElementsByTagName(g)[0];d.src=e;d.charset="utf-8";d.async=1;c.parentNode.insertBefore(d,c)};a["SeniverseWeatherWidgetObject"]=f;a[f]||(a[f]=function(){(a[f].q=a[f].q||[]).push(arguments)});a[f].l=+new Date();if(a.attachEvent){a.attachEvent("onload",b)}else{a.addEventListener("load",b,false)}}(window,document,"script","SeniverseWeatherWidget","//cdn.sencdn.com/widget2/static/js/bundle.js?t="+parseInt((new Date().getTime() / 100000000).toString(),10)));
-window.SeniverseWeatherWidget('show', {
-  flavor: "slim",
-  location: "WWE0TGW4PX6N",
-  geolocation: true,
-  language: "auto",
-  unit: "c",
-  theme: "blue",
-  token: "d46d6926-ce2b-4f0b-ad23-cfccb98ad469",
-  hover: "enabled",
-  container: "tp-weather-widget"
+  window.SeniverseWeatherWidget('show', {
+    flavor: "bubble",
+    location: "WWE0TGW4PX6N",
+    geolocation: true,
+    language: "auto",
+    unit: "c",
+    theme: "auto",
+    token: "d46d6926-ce2b-4f0b-ad23-cfccb98ad469",
+    hover: "enabled",
+    container: "tp-weather-widget"
 })
